f11acd715eca708ce9fd90cd541b4df01e2890f8
Ryan Shah <ryanshah@berkeley.edu>
2025-04-19 21:17:43 +0000

    updated files

--- homework/datagen.py
+++ homework/datagen.py
@@ -5,9 +5,21 @@ def generate_dataset(output_json: str, oversample: int = 10, temperature: float 
     #raise NotImplementedError()
 
     trainset = Dataset("train")
-    cot_model = CoTModel()
+    model = CoTModel()
 
-    prompts = ...
+    # a list of all the questions as strings
+    questions = [trainset[i][0] for i in range(len(trainset))]
+
+    # a list of the formatted prompts (1 per question)
+    prompts = [model.format_prompt(question) for question in questions]
+
+    # a list of lists - each sublist is all the generations for an individual question
+    generations = model.batched_generate(prompts, num_return_sequences=oversample, temperature=temperature)
+
+
+
+
+
 
 
 
